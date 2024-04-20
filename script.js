b5b9baff4efc805e40da0012ed16a397f4ab5aab
Oliv-19 <betza.bruguera@gmail.com>
2024-04-20 10:54:50 -0400

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+function getComputerChoice() {
+    pcChoice = Math.floor(Math.random() * 3) + 1;
+
+    if (pcChoice == 1) {
+        pcChoice= 'Rock'
+    } else if (pcChoice == 2){
+        pcChoice= 'Paper'
+    } else {
+        pcChoice= 'Scissors'
+    }
+    console.log(pcChoice);
+    return pcChoice
+}
+
+getComputerChoice()
